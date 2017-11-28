6025b1b669dc3da042eebd5464b59de99247ea25
80017 <arpitap@officebeacon.com>
2017-11-28 16:18:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -d '{"inServiceStrategy":{"launchConfig": {"imageUuid":"docker:nginx"}},"toServiceStrategy":null}' \
 'http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s380?action=upgrade'
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
--X POST \
-'http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s380?action=finishupgrade'
