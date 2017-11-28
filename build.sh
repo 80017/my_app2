bc1b4e5115095761022e073793295f16f613ceb1
80017 <arpitap@officebeacon.com>
2017-11-28 13:48:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
-body='{
- "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:nginx"}},"toServiceStrategy":null}'
-
-curl -u "F7C66CAD3B02315E7F76:bPWw74MRJ3G9UVdpbriiQUE5uxhRxqqNRiGXBXNu" \ 
- -X POST \
- -H "Content-Type: application/json" \
- -H "Accept: application/json" \
- -d "$body" \
- http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s380?action=upgrade
+curl -u "8275C9CC42066F469BF8:cL3Xc9ZQsngj1X8bsbGU7Ghje22zazdY55FJLZ7X" \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{"inServiceStrategy":{"launchConfig": {"tty":true,"vcpu":1,"imageUuid":"docker:rethinkdb"}},"toServiceStrategy":null}' \
+'http://172.16.61.162:8087/v2-beta/projects/1a5/services/1s31?action=upgrade'
