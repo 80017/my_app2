d571bb6b91bd6d31118125584e583f55559bf139
80017 <arpitap@officebeacon.com>
2017-11-30 16:18:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+curl -u "8275C9CC42066F469BF8:cL3Xc9ZQsngj1X8bsbGU7Ghje22zazdY55FJLZ7X" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
--d '{"inServiceStrategy":{"launchConfig": {"imageUuid":"docker:nginx"}},"toServiceStrategy":null}' \
-'http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s380?action=upgrade'
+-d '{"inServiceStrategy":{"launchConfig": {"tty":true,"vcpu":1,"imageUuid":"docker:rethinkdb"}},"toServiceStrategy":null}' \
+'http://172.16.61.162:8087/v2-beta/projects/1a5/services/1s31?action=upgrade'
 
+curl -u "8275C9CC42066F469BF8:cL3Xc9ZQsngj1X8bsbGU7Ghje22zazdY55FJLZ7X" \
+-X POST \
+'http://172.16.61.162:8087/v2-beta/projects/1a5/services/1s31?action=finishupgrade'
